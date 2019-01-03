1a15018ab17b6d5c25680a4e379cf1f4f37e377b
Muhamad-Andriansyah <45659337+Muhamad-Andriansyah@users.noreply.github.com>
2019-01-03 12:34:36 +0700

    Add files via upload

---
+++ Tugas Program Menghitung Upah Gaji Perjam Seorang Pegawai Jika Perjam @5000.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "MENGHITUNG UPAH GAJI PER JAM SEORANG PEGAWAI - MUHAMAD ANDRIANSYAH (NIM 311810297) TI18-B2" <<endl;
+
+    int gaji,j;
+    cout<<"masukan berapa jam anda kerja : ";
+    cin>>j;
+
+    //rumus
+    gaji = 5000 *j;
+    cout<<"gaji yang anda dapatkan adalah = " <<gaji;
+
+    return 0;
+}
